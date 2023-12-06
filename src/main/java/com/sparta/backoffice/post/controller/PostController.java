e5d5c97f0405f513c4c3046401c04b134c50d966
leeminju <leemj4090@naver.com>
2023-12-06 19:47:57 +0900

    refactor : 게시글 CRUD 사용자 인증 추가

--- src/main/java/com/sparta/backoffice/post/controller/PostController.java
+++ src/main/java/com/sparta/backoffice/post/controller/PostController.java
@@ -1,14 +1,18 @@
 package com.sparta.backoffice.post.controller;
 
+import com.sparta.backoffice.global.annotation.AuthUser;
 import com.sparta.backoffice.global.dto.BaseResponse;
 import com.sparta.backoffice.post.dto.PostRequestDto;
 import com.sparta.backoffice.post.dto.PostResponseDto;
 import com.sparta.backoffice.post.service.PostService;
+import com.sparta.backoffice.user.entity.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static com.sparta.backoffice.global.constant.ResponseCode.*;
 
 
 @RestController
@@ -18,39 +22,24 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity<BaseResponse<PostResponseDto>> createPost(@RequestBody @Valid PostRequestDto requestDto, Long id) {
-        PostResponseDto postResponseDto = postService.createPost(requestDto, id);
+    public ResponseEntity<BaseResponse<PostResponseDto>> createPost(@RequestBody @Valid PostRequestDto requestDto, @AuthUser User user) {
+        PostResponseDto postResponseDto = postService.createPost(requestDto, user);
         return ResponseEntity.status(HttpStatus.CREATED).body(
-                new BaseResponse<>(
-                        "게시글 작성 성공",
-                        HttpStatus.CREATED.value(),
-                        postResponseDto
-                )
-        );
+                BaseResponse.of(CREATED_POST, postResponseDto));
     }
 
     @PutMapping("/{postId}")
-    public ResponseEntity<BaseResponse<PostResponseDto>> updatePost(@PathVariable Long postId, @RequestBody @Valid PostRequestDto requestDto, Long id) {
-        PostResponseDto postResponseDto = postService.updatePost(requestDto, postId, id);
+    public ResponseEntity<BaseResponse<PostResponseDto>> updatePost(@PathVariable Long postId, @RequestBody @Valid PostRequestDto requestDto, @AuthUser User user) {
+        PostResponseDto postResponseDto = postService.updatePost(requestDto, postId, user);
         return ResponseEntity.status(HttpStatus.OK).body(
-                new BaseResponse<>(
-                        "게시글 수정 성공",
-                        HttpStatus.OK.value(),
-                        postResponseDto
-                )
-        );
+                BaseResponse.of(MODIFIYED_POST, postResponseDto));
     }
 
     @DeleteMapping("/{postId}")
-    public ResponseEntity<BaseResponse<Object>> updatePost(@PathVariable Long postId, Long id) {
-        postService.deletePost(postId, id);
+    public ResponseEntity<BaseResponse<Object>> updatePost(@PathVariable Long postId, @AuthUser User user) {
+        postService.deletePost(postId, user);
         return ResponseEntity.status(HttpStatus.OK).body(
-                new BaseResponse<>(
-                        "게시글 삭제 성공",
-                        HttpStatus.OK.value(),
-                        null
-                )
-        );
+                BaseResponse.of(DELETED_POST, null));
     }
 
 }
--- src/main/java/com/sparta/backoffice/post/service/PostService.java
+++ src/main/java/com/sparta/backoffice/post/service/PostService.java
@@ -1,35 +1,30 @@
 package com.sparta.backoffice.post.service;
 
-import com.sparta.backoffice.global.constant.ErrorCode;
+import com.sparta.backoffice.global.exception.ApiException;
 import com.sparta.backoffice.post.dto.PostRequestDto;
 import com.sparta.backoffice.post.dto.PostResponseDto;
 import com.sparta.backoffice.post.entity.Post;
 import com.sparta.backoffice.post.repository.PostRepository;
 import com.sparta.backoffice.user.entity.User;
-import com.sparta.backoffice.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.sparta.backoffice.global.constant.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
-    private final UserRepository userRepository;
 
-    public PostResponseDto createPost(PostRequestDto requestDto, Long userId) {
-        //임시로 user 가져오는 코드 -> 삭제 예정
-        User user = userRepository.findById(userId).orElseThrow(
-                () -> new NullPointerException(ErrorCode.NOT_FOUND_USER_ERROR.getMessage())
-        );
-
+    public PostResponseDto createPost(PostRequestDto requestDto, User user) {
         Post savedPost;
 
         if (requestDto.getParentPostId() != null) {
             //부모가 존재
             Post parentPost = postRepository.findById(requestDto.getParentPostId()).orElseThrow(
-                    () -> new NullPointerException("존재 하지 않는 게시글에 답글을 달 수 없습니다.")
-            );
+                    () -> new ApiException(CAN_NOT_REPLY_POST_ERROR));
+
             Post post = new Post(requestDto, parentPost, user);
             savedPost = postRepository.save(post);
         } else {
@@ -42,18 +37,12 @@ public class PostService {
     }
 
     @Transactional
-    public PostResponseDto updatePost(PostRequestDto requestDto, Long postId, Long userId) {
-        //임시로 user 가져오는 코드 -> 삭제 예정
-        User loginUser = userRepository.findById(userId).orElseThrow(
-                () -> new NullPointerException(ErrorCode.NOT_FOUND_USER_ERROR.getMessage())
-        );
-
+    public PostResponseDto updatePost(PostRequestDto requestDto, Long postId, User user) {
         Post post = postRepository.findById(postId).orElseThrow(
-                () -> new NullPointerException(ErrorCode.NOT_FOUND_POST_ERROR.getMessage())
-        );
+                () -> new ApiException(NOT_FOUND_POST_ERROR));
 
-        if (post.getUser().getId() != loginUser.getId()) {
-            throw new IllegalArgumentException("작성자만 수정할 수 있습니다.");
+        if (post.getUser().getId() != user.getId()) {
+            throw new ApiException(CAN_NOT_MODIFY_ERROR);
         }
 
         //내용 수정
@@ -62,18 +51,12 @@ public class PostService {
         return new PostResponseDto(post);
     }
 
-    public void deletePost(Long postId, Long userId) {
-        //임시로 user 가져오는 코드 -> 삭제 예정
-        User loginUser = userRepository.findById(userId).orElseThrow(
-                () -> new NullPointerException(ErrorCode.NOT_FOUND_USER_ERROR.getMessage())
-        );
-
+    public void deletePost(Long postId, User user) {
         Post post = postRepository.findById(postId).orElseThrow(
-                () -> new NullPointerException(ErrorCode.NOT_FOUND_POST_ERROR.getMessage())
-        );
+                () -> new ApiException(NOT_FOUND_POST_ERROR));
 
-        if (post.getUser().getId() != loginUser.getId()) {
-            throw new IllegalArgumentException("작성자만 삭제할 수 있습니다.");
+        if (post.getUser().getId() != user.getId()) {
+            throw new ApiException(CAN_NOT_DELETE_ERROR);
         }
 
         // 일단 연관 관게를 끊는 방식으로 구혐
