294192894da7922dd3b9c64380d8cae55f9e0944
MinJung Kim <kminss3711@gmail.com>
2023-12-06 12:02:04 +0900

    Feat: 공통 예외 및 응답 핸들러 추가

--- src/main/java/com/sparta/backoffice/global/constant/ErrorCode.java
+++ src/main/java/com/sparta/backoffice/global/constant/ErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
+    USER_NOT_FOUND(404, "유저를 찾을 수 없습니다."),
     /* 500 INTERNAL_SERVER_ERROR : 서버 에러 */
     INTERNAL_SERVER_ERROR(500, "내부 서버 에러입니다.");
 
