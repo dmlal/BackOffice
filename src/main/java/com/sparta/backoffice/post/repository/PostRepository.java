3dabe172ce8928d2f9f03cd02629ae4ca7ade5c9
OuOHoon <wls2579@gmail.com>
2023-12-07 21:44:46 +0900

    Feat(post): 팔로잉 유저들의 글 목록 조회 비공개 조건 변경 반영

--- src/main/java/com/sparta/backoffice/post/repository/PostRepository.java
+++ src/main/java/com/sparta/backoffice/post/repository/PostRepository.java
@@ -14,14 +14,11 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("select p from Post p where p.id in " +
             "(select p2.id from Like l join l.post p2 where l.user.id = :userId and p2.user.isPrivate = false " +
             "or (p2.user.isPrivate = true and p2.user.id in " +
-            "(select f.follower.id from Follow f where f.following.id = :userId)))")
+            "(select f.following.id from Follow f where f.follower.id = :userId)))")
     Page<Post> findPostsByLikes(Long userId, Pageable pageable);
 
     @Query("select p from Post p join p.user u where u.id in (" +
-            "select f.following.id from Follow f where f.follower.id = :userId and f.following.isPrivate = false " +
-            "or (f.following.isPrivate = true and f.following.id in (" +
-            "select f2.follower.id from Follow f2 where f2.following.id = :userId))" +
-            ") or u.id = :userId")
+            "select f.following.id from Follow f where f.follower.id = :userId) or u.id = :userId")
     Page<Post> findPostsByFollowingUsers(Long userId, Pageable pageable);
 
     Page<Post> findByUserAndParentPostIsNullAndIsDeletedFalse(User user, Pageable pageable);
