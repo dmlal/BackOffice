01cbe9a2822c6a1db252880440a454a7beecdde8
MinJung Kim <kminss3711@gmail.com>
2023-12-07 23:11:00 +0900

    Refactor: BaseResponse data 필드 null -> 빈 문자열로 변경

--- src/main/java/com/sparta/backoffice/global/aop/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/backoffice/global/aop/GlobalExceptionHandler.java
@@ -1,17 +1,14 @@
 package com.sparta.backoffice.global.aop;
 
-import com.sparta.backoffice.global.dto.BaseResponse;
-import com.sparta.backoffice.global.exception.ApiException;
+import static com.sparta.backoffice.global.constant.ErrorCode.*;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.HashMap;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,95 +16,97 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static com.sparta.backoffice.global.constant.ErrorCode.*;
+import com.sparta.backoffice.global.dto.BaseResponse;
+import com.sparta.backoffice.global.exception.ApiException;
 
-import java.util.HashMap;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    /**
-     * [Exception] RuntimeException 반환하는 경우
-     *
-     * @param ex RuntimeException
-     * @return ResponseEntity<BaseResponse>
-     */
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<BaseResponse<Void>> runtimeExceptionHandler(RuntimeException ex) {
-        log.error("Runtime Exceptions :", ex);
-        return ResponseEntity.internalServerError()
-                .body(
-                        BaseResponse.of(
-                                INTERNAL_SERVER_ERROR.getMessage(),
-                                INTERNAL_SERVER_ERROR.getHttpStatus(),
-                                null
-                        )
-                );
-    }
+	/**
+	 * [Exception] RuntimeException 반환하는 경우
+	 *
+	 * @param ex RuntimeException
+	 * @return ResponseEntity<BaseResponse>
+	 */
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<BaseResponse<String>> runtimeExceptionHandler(RuntimeException ex) {
+		log.error("Runtime Exceptions :", ex);
+		return ResponseEntity.internalServerError()
+			.body(
+				BaseResponse.of(
+					INTERNAL_SERVER_ERROR.getMessage(),
+					INTERNAL_SERVER_ERROR.getHttpStatus(),
+					""
+				)
+			);
+	}
 
-    /**
-     * [Exception] API 요청 시 '객체' 혹은 '파라미터' 데이터 값이 유효하지 않은 경우
-     *
-     * @param ex MethodArgumentNotValidException,
-     * @return ResponseEntity<BaseResponse>
-     */
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        log.error("handleMethodArgumentNotValidException", ex);
-        BindingResult bindingResult = ex.getBindingResult();
-        HashMap<String, String> errors = new HashMap<>();
-        bindingResult.getAllErrors()
-                .forEach(error -> errors.put(((FieldError) error).getField(), error.getDefaultMessage()));
+	/**
+	 * [Exception] API 요청 시 '객체' 혹은 '파라미터' 데이터 값이 유효하지 않은 경우
+	 *
+	 * @param ex MethodArgumentNotValidException,
+	 * @return ResponseEntity<BaseResponse>
+	 */
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+		MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		log.error("handleMethodArgumentNotValidException", ex);
+		BindingResult bindingResult = ex.getBindingResult();
+		HashMap<String, String> errors = new HashMap<>();
+		bindingResult.getAllErrors()
+			.forEach(error -> errors.put(((FieldError)error).getField(), error.getDefaultMessage()));
 
-        return ResponseEntity.badRequest()
-                .body(
-                        BaseResponse.of(
-                                INVALID_VALUE.getMessage(),
-                                INVALID_VALUE.getHttpStatus(),
-                                errors
-                        )
-                );
-    }
+		return ResponseEntity.badRequest()
+			.body(
+				BaseResponse.of(
+					INVALID_VALUE.getMessage(),
+					INVALID_VALUE.getHttpStatus(),
+					errors
+				)
+			);
+	}
 
-    /**
-     * [Exception] API 요청에 맞는 파라미터를 받지 못한 경우
-     *
-     * @param ex MissingServletRequestParameterException,
-     * @return ResponseEntity<BaseResponse>
-     */
-    @Override
-    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
-                                                                          HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+	/**
+	 * [Exception] API 요청에 맞는 파라미터를 받지 못한 경우
+	 *
+	 * @param ex MissingServletRequestParameterException,
+	 * @return ResponseEntity<BaseResponse>
+	 */
+	@Override
+	protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+		HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                BaseResponse.of(
-                        "누락된 파라미터 : " + ex.getParameterName(),
-                        INVALID_VALUE.getHttpStatus(),
-                        null
-                )
-        );
-    }
+		return ResponseEntity.badRequest()
+			.body(
+				BaseResponse.of(
+					"누락된 파라미터 : " + ex.getParameterName(),
+					INVALID_VALUE.getHttpStatus(),
+					""
+				)
+			);
+	}
 
-    /**
-     * [Exception] ApiException 반환하는 경우
-     * ErrorCode 를 사용하여 공통예외를 반환하는 경우 핸들링
-     *
-     * @param ex ApiException
-     * @return ResponseEntity<BaseResponse>
-     */
+	/**
+	 * [Exception] ApiException 반환하는 경우
+	 * ErrorCode 를 사용하여 공통예외를 반환하는 경우 핸들링
+	 *
+	 * @param ex ApiException
+	 * @return ResponseEntity<BaseResponse>
+	 */
 
-    @ExceptionHandler(ApiException.class)
-    public ResponseEntity<BaseResponse<Void>> apiExceptionHandler(ApiException ex) {
-        log.error("Runtime Exceptions :", ex);
-        return ResponseEntity.status(ex.getErrorCode().getHttpStatus())
-                .body(
-                        BaseResponse.of(
-                                ex.getErrorCode().getMessage(),
-                                ex.getErrorCode().getHttpStatus(),
-                                null
-                        )
-                );
-    }
+	@ExceptionHandler(ApiException.class)
+	public ResponseEntity<BaseResponse<String>> apiExceptionHandler(ApiException ex) {
+		log.error("Runtime Exceptions :", ex);
+		return ResponseEntity.status(ex.getErrorCode().getHttpStatus())
+			.body(
+				BaseResponse.of(
+					ex.getErrorCode().getMessage(),
+					ex.getErrorCode().getHttpStatus(),
+					""
+				)
+			);
+	}
 }
