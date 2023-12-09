e0e1ece145e6fc3c578c4db9011974ab301dda5f
leeminju <leemj4090@naver.com>
2023-12-10 02:40:32 +0900

    test : user swagger 코드 추가

--- src/main/java/com/sparta/backoffice/user/controller/UserReadController.java
+++ src/main/java/com/sparta/backoffice/user/controller/UserReadController.java
@@ -7,6 +7,14 @@ import com.sparta.backoffice.global.dto.BaseResponse;
 import com.sparta.backoffice.user.dto.UserInfoDto;
 import com.sparta.backoffice.user.entity.User;
 import com.sparta.backoffice.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User 조회 컨트롤러", description = "User 조회 컨트롤러 API")
 @RestController
 @RequestMapping("/api/read/users")
 public class UserReadController {
@@ -21,6 +30,26 @@ public class UserReadController {
     @Autowired
     private UserService userService;
 
+
+    @Operation(summary = "유저별 정보 조회", description = "모든 유저별 정보 조회 API",
+            parameters = {
+                    @Parameter(name = "userId", description = "조회할 사용자 번호", in = ParameterIn.PATH),
+                    @Parameter(name = "cursor", description = "페이지 번호", in = ParameterIn.QUERY),
+                    @Parameter(name = "size", description = "페이지에 표시할 게시물 개수", in = ParameterIn.QUERY),
+                    @Parameter(name = "dir", description = "정렬 방향", in = ParameterIn.QUERY)
+            })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "유저별 정보 조회 성공",
+                    content = @Content(schema = @Schema(implementation = BaseResponse.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "존재하지 않는 사용자 입니다.",
+                    content = @Content(schema = @Schema(implementation = BaseResponse.class))
+            )
+    })
     @GetMapping("/{userId}")
     public ResponseEntity<BaseResponse<UserInfoDto>> getUserInfo(@PathVariable Long userId, @AuthUser User user) {
         UserInfoDto userInfo = userService.getUserInfo(userId, user);
--- src/main/java/com/sparta/backoffice/user/dto/request/PasswordUpdateRequestDto.java
+++ src/main/java/com/sparta/backoffice/user/dto/request/PasswordUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package com.sparta.backoffice.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
@@ -15,6 +16,7 @@ public class PasswordUpdateRequestDto {
             regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-])[a-zA-Z0-9@#$%^&+=!]*$",
             message = "비밀번호는 영어 대/소문자, 숫자, 특수문자의 조합으로 입력해야합니다."
     )
+    @Schema(description = "변경할 비밀번호", nullable = false, example = "!!User1111")
     private final String newPassword;
 }
 
