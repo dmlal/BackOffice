9f5006067962b5e52947a81692da3e6701a2d00d
MinJung Kim <kminss3711@gmail.com>
2023-12-10 18:54:28 +0900

    refactor: Factory class private 생성자 추가
    인스턴스 생성 방지하기위한 private 생성자 추가

--- src/main/java/com/sparta/backoffice/global/security/oauth/user/OAuth2UserInfoFactory.java
+++ src/main/java/com/sparta/backoffice/global/security/oauth/user/OAuth2UserInfoFactory.java
@@ -5,6 +5,9 @@ import java.util.Map;
 import com.sparta.backoffice.global.constant.SocialType;
 
 public class OAuth2UserInfoFactory {
+	private OAuth2UserInfoFactory() {
+	}
+
 	public static OAuth2UserDetails getOAuth2UserInfo(SocialType socialType, Map<String, Object> attributes) {
 		return switch (socialType) {
 			case NAVER -> new NaverOAuth2UserDetails(attributes);
